89ba91466938c060b3b29813df99ed0042676107
axetroy <troy450409405@gmail.com>
2017-03-13 11:47:48 +0800

    feat: if have install yarn, and the project have yarn.lock, then run [yarn] instead of [npm install]

--- index.js
+++ index.js
@@ -1,14 +1,61 @@
 /**
  * Created by axetroy on 17-3-10.
  */
-
+const path = require('path');
 const spawn = require('child_process').spawn;
+const Promise = require('bluebird');
+const co = require('co');
+const fs = Promise.promisifyAll(require('fs-extra'));
+const which$0 = require('which');
 
-function npmInstall(done) {
-  const ls = spawn('npm', ['install'], {stdio: 'inherit'});
-  ls.on('close', (code) => {
-    done();
+function isYarnEnv() {
+  return co(function *() {
+    yield [
+      fs.readFileAsync(path.join('yarn.lock')),
+      which$1('yarn')
+    ];
+    return true;
+  }).catch((err) => {
+    console.error(err);
+    return Promise.resolve(false);
   });
 }
 
-exports.add = npmInstall;
+function which$1(cmd, options = {}) {
+  return new Promise(function (resolve, reject) {
+    which$0.call(this, cmd, options, function (err, resolvePath) {
+      if (err) return reject(err);
+      resolve(resolvePath);
+    });
+  });
+}
+
+function nodeModuleInstall(done) {
+
+  co(function*() {
+
+    try {
+      yield fs.readJsonAsync(path.join('package.json'));
+    } catch (err) {
+      throw new Error('Invalid package.json');
+    }
+
+    const __isYarnEnv__ = yield isYarnEnv();
+
+    let cmd = [];
+
+    if (__isYarnEnv__) {
+      cmd = ['yarn']
+    } else {
+      cmd = ['npm', 'install']
+    }
+
+    const ls = spawn(cmd.shift(), cmd, {stdio: 'inherit'});
+    ls.on('close', (code, signal) => {
+      code === 0 ? done() : done(new Error(`Error Code: ${code}, Exist Signal: ${signal}`));
+    });
+
+  }).catch((err) => done(err));
+}
+
+exports.add = nodeModuleInstall;
